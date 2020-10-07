ce6cd17a5672cc137656f63170ef27b0c0289698
rifqisambas <rifqi@sera5.id>
2020-10-07 16:42:55 +0700

    init

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class HomeController extends Controller
+{
+	public function index()
+	{
+		$anggota = Http::get('https://sera5.id/pensiunan/api/anggota')->body();
+		$data = json_decode($anggota)->data;
+
+		return view('anggota', ['data' => $data]);
+	}
+
+	public function import(Request $request)
+	{
+		if ($request->hasFile('file')) {
+			Http::attach('file', fopen($request->file, 'r'))->post('https://sera5.id/pensiunan/api/anggota/import');
+		}
+
+		return redirect()->action('\App\Http\Controllers\HomeController@index');
+	}
+
+	public function create(Request $request)
+	{
+		Http::post('https://sera5.id/pensiunan/api/anggota', [
+			'branch_code' 			=> $request->input('branch_code'),
+			'branch_location' 	=> $request->input('branch_location'),
+			'loan_type' 				=> $request->input('loan_type'),
+			'deal_ref' 					=> $request->input('deal_ref'),
+			'short_name' 				=> $request->input('short_name'),
+			'start_date' 				=> $request->input('start_date'),
+			'os' 								=> $request->input('os')
+		]);
+
+		return redirect()->action('\App\Http\Controllers\HomeController@index');
+	}
+}
---
+++ resources/views/anggota.blade.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Pensiunan</title>
+	<style>
+		body {
+			margin-left: 15%;
+			margin-right: 15%;
+		}
+	</style>
+</head>
+
+<body>
+	<h1>Anggota Pensiunan</h1>
+
+	<form action="/import" method="post" enctype="multipart/form-data">
+		<div>
+			<strong>import data</strong>
+		</div>
+		@csrf
+		<input type="file" name="file" required>
+		<input type="submit" value="import">
+	</form>
+
+	<br>
+
+	<div>
+		<strong>create data</strong>
+	</div>
+	<form action="/create" method="post">
+		@csrf
+		<div>
+			<input type="text" name="branch_code" placeholder="branch code" required>
+		</div>
+		<div>
+			<input type="text" name="branch_location" placeholder="branch location" required>
+		</div>
+		<div>
+			<input type="text" name="loan_type" placeholder="loan type" required>
+		</div>
+		<div>
+			<input type="number" name="deal_ref" placeholder="deal ref" required>
+		</div>
+		<div>
+			<input type="text" name="short_name" placeholder="short name" required>
+		</div>
+		<div>
+			<input type="date" name="start_date" placeholder="start date" required>
+		</div>
+		<div>
+			<input type="number" name="os" placeholder="os" required>
+		</div>
+		<div>
+			<input type="submit" value="create">
+		</div>
+	</form>
+
+	<br>
+
+	<table border=1>
+		<div><strong>Daftar Anggota</strong></div>
+		<thead>
+			<tr>
+				<th colspan="2">Branch</th>
+				<th rowspan="2">Loan Type</th>
+				<th rowspan="2">Deal Ref</th>
+				<th rowspan="2">Short Name</th>
+				<th rowspan="2">Start Data</th>
+				<th rowspan="2">OS</th>
+			</tr>
+			<tr>
+				<th>Code</th>
+				<th>Location</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($data as $data)
+			<tr>
+				<td>{{$data->branch_code}}</td>
+				<td>{{$data->branch_location}}</td>
+				<td>{{$data->loan_type}}</td>
+				<td>{{$data->deal_ref}}</td>
+				<td>{{$data->short_name}}</td>
+				<td>{{$data->start_date}}</td>
+				<td>{{$data->os}}</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+</body>
+
+</html>
